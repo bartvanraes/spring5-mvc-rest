db60b4d5f0382bc4ca3190c805e991ea112757ed
ASUS-EMPEROR\Bart <bart.vanraes@gmail.com>
2020-03-16 11:23:29 +0100

    Mapstruct upgraded

--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -1,10 +1,10 @@
 package guru.springfamework.controllers.v1;
 
-import guru.springfamework.api.v1.model.VendorDTO;
 import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.services.VendorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import guru.springfamework.api.v1.model.VendorDTO;
 
 @RestController
 @RequestMapping(VendorController.BASE_URL)
